1f6b89fb99b61bbcc4f785651b68eb094bd6b67f
Adeniyi Ridwan <adeniyiridwan117@gmail.com>
2022-10-31 16:32:00 -0700

    String

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -13,16 +13,16 @@ void print_diagsums(int *a, int size)
 	/*Declaring variables*/
 	int i;
 	int sum1, sum2;
-	
+
 	sum1 = 0;
 	sum2 = 0;
-	
+
 	i = 0;
 	while (i < size) /*number repetitions*/
 	{											/*sums for diagsums*/
 		sum1 = sum1 + *(a + i * size + i);
 		sum2 = sum2 + *(a + i * size + size - i - 1);
-							
+
 		i++; /*add +1*/
 	}
 
