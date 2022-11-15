5139aed17e3cd39838086a3e7f8345f97dbc267c
Adeniyi Ridwan <adeniyiridwan117@gmail.com>
2022-11-14 19:18:29 -0800

    In Camp

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,13 +1,32 @@
-
-#ifndef SUM_H
-#define SUM_H
+#ifndef DOG_H
+#define DOG_H
 
 /*
- * File: 4-sum.h
- * Desc: Header file that defines a function-like macro SUM(x, y)
- *       that computes the sum of the numbers x and y.
+ * File: dog.h
+ * Desc: Header file that defines a new type struct dog.
  */
 
-#define SUM(x, y) ((x) + (y))
+/**
+ * struct dog - A new type describing a dog.
+ * @name: The name of the dog.
+ * @age: The age of the dog.
+ * @owner: The owner of the dog.
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+/**
+ * dog_t - Typedef for struct dog
+ */
+typedef struct dog dog_t;
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
 
 #endif
